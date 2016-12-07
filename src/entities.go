89dd5d0b26aff159da26ecb54d54edc42f006dae
darkoverlordofdata <darkoverlordofdata@gmail.com>
2016-12-06 21:52:05 -0800

    daily

--- src/entities.go
+++ src/entities.go
@@ -57,7 +57,7 @@ type Entity struct {
 }
 
 // CreateEntity
-func (this *ShmupWarz) CreateEntity(mask uint64) (e *Entity) {
+func CreateEntity(mask uint64) (e *Entity) {
 	e = new(Entity)
 	e.Id = GetEntityId()
 	e.Mask = mask | C_LAYER | C_POSITION | C_RESOURCE
@@ -66,40 +66,44 @@ func (this *ShmupWarz) CreateEntity(mask uint64) (e *Entity) {
 
 // CreatePlayer
 func (this *ShmupWarz) CreatePlayer(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_PLAYER | C_BOUNDS | C_HEALTH)
-	e.Layer.Ordinal = LAYER_PLAYER
+	e = CreateEntity(C_PLAYER | C_BOUNDS | C_HEALTH)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_PLAYER}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/fighter.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Bounds.Radius = 45
+	e.Resource = ResourceComponent{Name: "assets/images/fighter.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Bounds = BoundsComponent{Radius: 45}
 	e.Health = HealthComponent{MaxHealth: 100, MinHealth: 100}
-	e.Player.Active = true
+	e.Player = PlayerComponent{Active: true}
 	return e
 }
 
 // CreateBullet
 func (this *ShmupWarz) CreateBullet(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_BULLET | C_BOUNDS | C_EXPIRES | C_TINT | C_VELOCITY)
-	e.Layer.Ordinal = LAYER_BULLET
+	e = CreateEntity(C_BULLET | C_BOUNDS | C_EXPIRES | C_TINT | C_VELOCITY)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_BULLET}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/bullet.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Bounds.Radius = 3
-	e.Expires.Delay = 2
+	e.Resource = ResourceComponent{Name: "assets/images/bullet.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Bounds = BoundsComponent{Radius: 3}
+	e.Expires = ExpiresComponent{Delay: 2}
+	e.Health = HealthComponent{MaxHealth: 100, MinHealth: 100}
 	e.Velocity = VelocityComponent{X: 0, Y: -800}
 	e.Tint = TintComponent{R: 0xad, G: 0xff, B: 0x2f, A: 255}
-	e.Bullet.Active = true
+	e.Bullet = BulletComponent{Active: true}
 	return e
 }
 
 // CreateEnemy1
 func (this *ShmupWarz) CreateEnemy1(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
-	e.Layer.Ordinal = LAYER_ENEMY1
+	e = CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_ENEMY1}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/enemy1.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Bounds.Radius = 35
+	e.Resource = ResourceComponent{Name: "assets/images/enemy1.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Bounds = BoundsComponent{Radius: 35}
 	e.Health = HealthComponent{MaxHealth: 10, MinHealth: 10}
 	e.Velocity = VelocityComponent{X: 0, Y: 40}
 	e.Enemy.Active = true
@@ -108,12 +112,13 @@ func (this *ShmupWarz) CreateEnemy1(x float64, y float64) (e *Entity) {
 
 // CreateEnemy2
 func (this *ShmupWarz) CreateEnemy2(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
-	e.Layer.Ordinal = LAYER_ENEMY2
+	e = CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_ENEMY2}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/enemy2.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Bounds.Radius = 86
+	e.Resource = ResourceComponent{Name: "assets/images/enemy2.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Bounds = BoundsComponent{Radius: 86}
 	e.Health = HealthComponent{MaxHealth: 20, MinHealth: 20}
 	e.Velocity = VelocityComponent{X: 0, Y: 30}
 	e.Enemy.Active = true
@@ -122,12 +127,13 @@ func (this *ShmupWarz) CreateEnemy2(x float64, y float64) (e *Entity) {
 
 // CreateEnemy3
 func (this *ShmupWarz) CreateEnemy3(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
-	e.Layer.Ordinal = LAYER_ENEMY3
+	e = CreateEntity(C_ENEMY | C_BOUNDS | C_HEALTH | C_VELOCITY)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_ENEMY3}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/enemy3.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Bounds.Radius = 100
+	e.Resource = ResourceComponent{Name: "assets/images/enemy3.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Bounds = BoundsComponent{Radius: 100}
 	e.Health = HealthComponent{MaxHealth: 40, MinHealth: 40}
 	e.Velocity = VelocityComponent{X: 0, Y: 20}
 	e.Enemy.Active = true
@@ -136,12 +142,13 @@ func (this *ShmupWarz) CreateEnemy3(x float64, y float64) (e *Entity) {
 
 // CreateExplosion
 func (this *ShmupWarz) CreateExplosion(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_EXPIRES | C_SCALE | C_SCALETWEEN)
-	e.Layer.Ordinal = LAYER_EXPLOSION
+	e = CreateEntity(C_EXPIRES | C_SCALE | C_SCALETWEEN)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_EXPLOSION}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/explosion.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Expires.Delay = 0.5
+	e.Resource = ResourceComponent{Name: "assets/images/explosion.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Expires = ExpiresComponent{Delay: 0.5}
 	e.Scale = ScaleComponent{X: 1, Y: 1}
 	e.ScaleTween = ScaleTweenComponent{Min: .001, Max: 1, Speed: -3, Repeat: false, Active: true}
 	return e
@@ -149,12 +156,13 @@ func (this *ShmupWarz) CreateExplosion(x float64, y float64) (e *Entity) {
 
 // CreateBang
 func (this *ShmupWarz) CreateBang(x float64, y float64) (e *Entity) {
-	e = this.CreateEntity(C_EXPIRES | C_SCALE | C_SCALETWEEN)
-	e.Layer.Ordinal = LAYER_EXPLOSION
+	e = CreateEntity(C_EXPIRES | C_SCALE | C_SCALETWEEN)
+
+	e.Layer = LayerComponent{Ordinal: LAYER_EXPLOSION}
 	e.Position = PositionComponent{X: x, Y: y}
-	e.Resource.Name = "assets/images/bang.png"
-	e.Sprite.Texture = this.LoadTexture(e.Resource.Name)
-	e.Expires.Delay = 0.5
+	e.Resource = ResourceComponent{Name: "assets/images/bang.png"}
+	e.Sprite = SpriteComponent{Texture: this.LoadTexture(e.Resource.Name)}
+	e.Expires = ExpiresComponent{Delay: 0.5}
 	e.Scale = ScaleComponent{X: 1, Y: 1}
 	e.ScaleTween = ScaleTweenComponent{Min: .001, Max: 1, Speed: -3, Repeat: false, Active: true}
 	return e
